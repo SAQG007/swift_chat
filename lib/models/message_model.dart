ad70369cea093d3f3e637aa8bf73f1de243e0f6c
SAQG007 <syedabdulqadirgillani807@gmail.com>
2023-09-26 17:51:18 +0500

    Designed message model

--- lib/models/message_model.dart
+++ lib/models/message_model.dart
@@ -1,10 +1,9 @@
 class MessageModel {
   String type;
   String message;
-  String time;
+  
   MessageModel({
     required this.message,
-    required this.type,
-    required this.time
+    required this.type
   });
 }
